1938ab10285f2ba949282981bde9f70518688095
faldea1 <faldea1@uc.cl>
2022-12-19 05:24:11 +0000

    Project Traffic Light Ready

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+//MAIN COMPONENT
 const TrafficLight = () => {
+	const [ ActualColor, ChangeColor ] = useState("red");
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="trafficlight d-flex justify-content-center">
+			<div onClick={() => ChangeColor ("red")} className={"light red"+((ActualColor === "red") ? " highlight": "")}></div>
+			<div onClick={() => ChangeColor ("yellow")} className={"light yellow"+((ActualColor === "yellow") ? " highlight": "")}></div>
+			<div onClick={() => ChangeColor ("green")} className={"light green"+((ActualColor === "green") ? " highlight": "")}></div>
 		</div>
 	);
 };
 
 export default TrafficLight;
+
+
+//UseEffect -> default en rojo. Llamada a ChangeColor para renderizar nuevo color, mediante click.
